ee8baae65d78d2d350b3a74cc4d3ea5283a7a3d0
danbiya <dhika.anbiya@gmail.com>
2017-04-06 17:37:38 +0700

    add new route for logout

--- routes/api.php
+++ routes/api.php
@@ -9,6 +9,7 @@ use App\User;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -39,7 +40,7 @@ $records =
             )
        ) AS distance")
     )
-    ->having("distance", "<", "5")
+    ->having("distance", "<", "10")
     ->orderBy("distance")
     ->offset(0)
     ->limit(10)
@@ -98,6 +99,30 @@ $records =
 return response()->json(array('status'=>'true','data'=>$records));
 })->middleware('ath');
 
+Route::get('v1/restaurant', function(Request $request){
+$lat = $request->header('lat');
+$lng = $request->header('lng');
+$records = 
+    Restaurant::select(
+        DB::raw("name, address, phone, image, lat, lng,( 
+            6371 * acos( 
+                cos( radians(  ?  ) ) *
+                cos( radians( lat ) ) * 
+                cos( radians( lng ) - radians(?) ) + 
+                sin( radians(  ?  ) ) *
+                sin( radians( lat ) ) 
+            )
+       ) AS distance")
+    )
+    ->having("distance", "<", "10")
+    ->orderBy("distance")
+    ->offset(0)
+    ->limit(10)
+    ->setBindings([$lat, $lng, $lat])
+    ->get();
+return response()->json(array('status'=>'true','data'=>$records));
+})->middleware('ath');
+
 Route::get('v1/site', function(Request $request){
 $lat = $request->header('lat');
 $lng = $request->header('lng');
@@ -122,26 +147,31 @@ $records =
 return response()->json(array('status'=>'true','data'=>$records));
 })->middleware('ath');
 
-Route::get('v1/hello', function(Request $request){
-	return response()->json('hi');
-})->middleware('ath');
-
-Route::get('v1/restaurant_menu', function(Request $request){
-$restaurant_id = $request->header('restaurant_id');
-$records = 
-    DB::table('menus')->where(
-    	'restaurant_id',$restaurant_id
-    	)->get();
-return response()->json(array('status'=>'true','data'=>$records));
-})->middleware('ath');
 
 Route::post('v1/login', function(Request $request){
-	
 	$email = $request->header('email');
 	$password = $request->header('password');
 	if (Auth::attempt(['email' => $email, 'password' => $password])) {
         $records = User::where('email', $email)->firstOrFail();
         return response()->json(array('status'=>'true','data'=>$records));
-    };
+    }else{
+    	$records=array('message'=>'error, please check your username and password');
+    	return response()->json(array('status'=>'false','data'=>$records));
+    }
 });
+
+Route::post('v1/logout',function(Request $request){
+
+		$api_token = $request->header('api_token');
+	
+		$token = Str::random(20);        
+		$update = User::where('api_token',$api_token)->firstOrFail();
+		$update->api_token = $token;
+		if($update->save()){
+			return response()->json(array('status'=>'true','data'=>array('message'=>'logout success')));
+		}else{
+					return response()->json(array('status'=>'false','data'=>array('message'=>'logout failed')));			
+		}
+})->middleware('ath');
+
 
