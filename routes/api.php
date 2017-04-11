1f16766ef734fd8e8e0baf4895c015218dd49249
danbiya <dhika.anbiya@gmail.com>
2017-04-11 10:41:19 +0700

    change all method to post

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Illuminate\Validation\Validator;
+
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -25,9 +28,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('v1/fuel', function(Request $request){
-$lat = $request->header('lat');
-$lng = $request->header('lng');
+Route::post('v1/fuel', function(Request $request){
+$lat = $request->input('lat');
+$lng = $request->input('lng');
 $records = 
     Fuel::select(
         DB::raw("name, lat, lng,( 
@@ -50,9 +53,9 @@ return response()->json(array('status'=>'true','data'=>$records));
 })->middleware('ath');
 
 
-Route::get('v1/office', function(Request $request){
-$lat = $request->header('lat');
-$lng = $request->header('lng');
+Route::post('v1/office', function(Request $request){
+$lat = $request->input('lat');
+$lng = $request->input('lng');
 $records = 
     Office::select(
         DB::raw("name, address, phone, information, image, lat, lng,( 
@@ -75,9 +78,9 @@ return response()->json(array('status'=>'true','data'=>$records));
 })->middleware('ath');
 
 
-Route::get('v1/worship', function(Request $request){
-$lat = $request->header('lat');
-$lng = $request->header('lng');
+Route::post('v1/worship', function(Request $request){
+$lat = $request->input('lat');
+$lng = $request->input('lng');
 $records = 
     Worship::select(
         DB::raw("name, address, religion_type, image, lat, lng,( 
@@ -99,9 +102,9 @@ $records =
 return response()->json(array('status'=>'true','data'=>$records));
 })->middleware('ath');
 
-Route::get('v1/restaurant', function(Request $request){
-$lat = $request->header('lat');
-$lng = $request->header('lng');
+Route::post('v1/restaurant', function(Request $request){
+$lat = $request->input('lat');
+$lng = $request->input('lng');
 $records = 
     Restaurant::select(
         DB::raw("name, address, phone, image, lat, lng,( 
@@ -123,9 +126,16 @@ $records =
 return response()->json(array('status'=>'true','data'=>$records));
 })->middleware('ath');
 
-Route::get('v1/site', function(Request $request){
-$lat = $request->header('lat');
-$lng = $request->header('lng');
+Route::post('v1/restaurant_menu', function(Request $request){
+$restaurant_id = $request->input('restaurant_id');
+
+$records = Menu::where('restaurant_id',$restaurant_id)->firstOrFail();
+return response()->json(array('status'=>'true','data'=>$records));
+})->middleware('ath');
+
+Route::post('v1/site', function(Request $request){
+$lat = $request->input('lat');
+$lng = $request->input('lng');
 $records = 
     Site::select(
         DB::raw("name, operation_time, facility, image, lat, lng,( 
@@ -149,11 +159,21 @@ return response()->json(array('status'=>'true','data'=>$records));
 
 
 Route::post('v1/login', function(Request $request){
-	$email = $request->header('email');
-	$password = $request->header('password');
+	$email = $request->input('email');
+	$password = $request->input('password');
+    $api_token = $request->header('api_token');
+
 	if (Auth::attempt(['email' => $email, 'password' => $password])) {
-        $records = User::where('email', $email)->firstOrFail();
-        return response()->json(array('status'=>'true','data'=>$records));
+        $token = Str::random(20);    
+        $update = User::where('email',$email)->firstOrFail();
+        $update->api_token = $token;  
+        if($update->save()){
+            $records = User::where('email', $email)->firstOrFail();
+            return response()->json(array('status'=>'true','data'=>$records));    
+        }else{
+            return response()->json(array('status'=>'true','data'=>array('message'=>'login error')));    
+        }
+        
     }else{
     	$records=array('message'=>'error, please check your username and password');
     	return response()->json(array('status'=>'false','data'=>$records));
@@ -170,8 +190,36 @@ Route::post('v1/logout',function(Request $request){
 		if($update->save()){
 			return response()->json(array('status'=>'true','data'=>array('message'=>'logout success')));
 		}else{
-					return response()->json(array('status'=>'false','data'=>array('message'=>'logout failed')));			
+		return response()->json(array('status'=>'false','data'=>array('message'=>'logout failed')));			
 		}
 })->middleware('ath');
+
+Route::post('v1/register',function(Request $request){
+       
+       $name = $request->input('name');
+       $email = $request->input('email');
+       $password = $request->input('password');        
+       
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'bail|required|unique:users',
+            'password' => 'required'
+        ]);
+
+       
+        $token = Str::random(20);        
+
+        $save = new User;
+        $save->name = $name;
+        $save->password = $password;
+        $save->email = $email;
+        $save->api_token = $token;
+
+        if($save->save()){
+            return response()->json(array('status'=>'true','data'=>array('message'=>'register success')));
+        }else{
+            return response()->json(array('status'=>'false','data'=>array('message'=>'logout failed','error'=>$errors->all())));            
+        }
+});
 
 
